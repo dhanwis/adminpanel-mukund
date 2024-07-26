0ed7a873c3166a94a323d5def6a307d72be071ea
sreeharisreehari <kisreehari52@gmail.com>
2024-07-26 14:19:59 +0530

    image addding

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -2,6 +2,7 @@ import { BASE_URL } from "./baseurl"
 import { commonAPI } from "./commonAPI"
 
 
+
 //login
 export const loginAPI=async(user)=>{
     return await commonAPI('POST',`${BASE_URL}/user/login`,user,"")
