075215a32349933e10143258802291e5f55eae54
sreeharisreehari <kisreehari52@gmail.com>
2024-09-12 10:22:32 +0530

    new render host

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,5 +1,5 @@
-   export const BASE_URL= 'http://localhost:5000'
+   // export const BASE_URL= 'http://localhost:5000'
 
   //  export const BASE_URL= 'https://mukund-server-side.onrender.com'     
 
-
+  export const BASE_URL= 'https://mukund-server-side-6k75.onrender.com'     
